785c096dae0fbbd0b765c98b704e006b3bf3723b
pruthvinc05 <chalasepruthvi@gmail.com>
2026-08-12 21:51:14 +0530

    Add clerk auth:sign up, sign in, OTP, device trust

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,33 @@
+import { useAuth, useUser } from '@clerk/expo'
+import { useRouter } from 'expo-router'
+import React from 'react'
+import { Alert, Text, TouchableOpacity } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function ProfileScreen() {
+    const { user } = useUser()
+    const { signOut } = useAuth()
+    const router = useRouter()
+
+    const handleSignOut = () => {
+        Alert.alert("Sign out", "Are you sure you want to sign out?", [
+            { text: "Cancel", style: "cancel" },
+            {
+                text: "Sign out",
+                style: "destructive",
+                onPress: async () => {
+                    await signOut()
+                    router.replace("/sign-in")
+                }
+            }
+        ])
+    }
+
+    return (
+        <SafeAreaView className='flex-1 bg-brand-body' edges={["top"]}>
+            <TouchableOpacity onPress={handleSignOut}>
+                <Text>Log out</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
+}
